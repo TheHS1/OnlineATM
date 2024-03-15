6d29ca8117e4658e5a22f2e88cbb184a0458adb8
Harjot Singh <harjot.singh0712@gmail.com>
2024-03-15 15:51:02 -0700

    Use Django-provided user model instead

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,19 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 import uuid
 
-class Users(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200, null=False)
-    phone_number = models.CharField(max_length=15, null=False)
-    address = models.CharField(max_length=200, null=False)
-    email = models.EmailField(max_length=200, null=False)
-
-    def __str__(self):
-        return self.name
-
 class Accounts(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, null=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     balance = models.IntegerField(default=0, null=False)
     date_opened = models.DateField(auto_now_add = True, null=False)
     account_type = models.CharField(max_length=50, null=False)
