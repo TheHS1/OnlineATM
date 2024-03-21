7118a59c75c145e12fcfb34deef8c7aac4f81651
Ihsan <ihsan510d@gmail.com>
2024-03-21 12:38:25 -0700

    implemented login and registration

--- mainApp/models.py
+++ mainApp/models.py
@@ -3,10 +3,13 @@ import uuid
 
 class Users(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=200, null=False)
-    phone_number = models.CharField(max_length=15, null=False)
-    address = models.CharField(max_length=200, null=False)
-    email = models.EmailField(max_length=200, null=False)
+    first_name = models.CharField(max_length=200, null=False, default='')
+    last_name = models.CharField(max_length=200, null=False, default='')
+    password = models.CharField(max_length=200, null=False, default='')
+    phone_number = models.CharField(max_length=15, null=False, default='')
+    address = models.CharField(max_length=200, null=False, default='')
+    email = models.EmailField(max_length=200, null=False, default='@.com')
+    pin = models.CharField(max_length=4, default='')
 
     def __str__(self):
         return self.name
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,16 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.mail import EmailMultiAlternatives
+from django.http import HttpResponse
 from .forms import LoginForm
 from .forms import RegisterForm
+from .forms import ModelForm
+from .forms import Users
+# from django.forms import Form
 
 # Create your views here.
 
 def home(request):
-    form = LoginForm()
+    if request.method == "POST":
+        print("test1")
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            print("test2")
+            emailLogin = form.cleaned_data['email']
+            passwordLogin = form.cleaned_data['password']
+            user = authenticate(request, email=emailLogin, password=passwordLogin)
+            
+            if user is not None:
+                # login(request, user)
+                print("success")
+            else:
+                pass
+    else:
+        form = LoginForm()
     return render(request, 'home.html', {'form': form})
 
 def register_view(request):
-    form = RegisterForm()
-    return render(request, 'register_view.html', {'form':form})
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+
+            # data = form.cleaned_data
+            # user_profile = Users(
+            #     first_name=data['first_name'],
+            #     last_name=data['last_name'],
+            #     email=data['email'],
+            #     password=data['password'],
+            #     address=data['address'],
+            #     pin=data['pin'],
+            #     phone_number=data['phone_number'],
+            # )
+            # user_profile.save()
+            form.save()
+            return redirect('home')
+        else:
+            print(form.errors)
+    else:
+        print("test4")
+        form = RegisterForm()
+
+    return render(request, 'register_view.html', {'form': form})
 
 def customer_view(request):
     return render(request, 'customer_view.html')
