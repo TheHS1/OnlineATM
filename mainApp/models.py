6a1e2a3011d731b79771bcc6021ee52316213260
Harjot Singh <harjot.singh0712@gmail.com>
2024-03-23 14:01:12 -0700

    Update Users model to extend Django AbstractUser

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 import uuid
 
-class Users(models.Model):
+class Users(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=200, null=False, default='')
-    last_name = models.CharField(max_length=200, null=False, default='')
-    password = models.CharField(max_length=200, null=False, default='')
     phone_number = models.CharField(max_length=15, null=False, default='')
     address = models.CharField(max_length=200, null=False, default='')
-    email = models.EmailField(max_length=200, null=False, default='@.com')
+    email = models.EmailField(max_length=100, unique=True, default='')
     pin = models.CharField(max_length=4, default='')
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [pin]
 
     def __str__(self):
-        return self.name
+        return self.first_name + " " + self.last_name
 
 class Accounts(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
